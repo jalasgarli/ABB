d34c37b6b079773c6029f737a272968baf51d906
javidalasgarli <alasgarlijavid@gmail.com>
2024-02-08 15:14:55 +0400

    Task 2 Added

--- Exercise_5/Task_1/Main.java
+++ Exercise_5/Task_1/Main.java
@@ -2,6 +2,10 @@ package Exercise_5.Task_1;
 
 public class Main {
     public static void main(String[] args) {
-        
+        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee("Ali", 0, 2000.50);
+        PartTimeEmployee partTimeEmployee = new PartTimeEmployee("Nijat", 1, 25.5, 20);
+        System.out.println("Salary of Full Time Employee: " + fullTimeEmployee.calculateSalary());
+        System.out.println("Salary of Full Time Employee: " + partTimeEmployee.calculateSalary());
     }
 }
+
---
+++ Exercise_5/Task_2/Outer.java
@@ -0,0 +1,23 @@
+package Exercise_5.Task_2;
+
+public class Outer {
+    private static class Inner {
+        private static int count = 0;
+
+        public Inner() {
+            count++;
+        }
+
+        public static int getCount() {
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Inner inner1 = new Inner();
+        Inner inner2 = new Inner();
+        Inner inner3 = new Inner();
+
+        System.out.println(Inner.getCount());
+    }
+}
